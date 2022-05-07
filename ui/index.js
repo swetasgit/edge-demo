07be3bd7335dcb679ff694277f4cc4d581ea0e21
Andrew Gracey <andrew.gracey@suse.com>
2022-05-07 14:40:14 -0700

    added promotion lookup and passthrough of more data

--- ui/index.js
+++ ui/index.js
@@ -10,6 +10,23 @@ const { Server } = require("socket.io")
 const io = new Server(server)
 
 
+// TODO make envVar...
+const redis = require('redis');
+const client = redis.createClient({
+    url: 'redis://:Password@edge-demo-redis-headless.redis:6379/0'
+})
+
+client.on('error', err => {
+    console.log('Redis Error ' + err);
+})
+
+const setup = async ()=>{
+    await client.connect()
+    client.hSet('active_promotions', 'suse_chameleon', '20% Discount to SLES Subscriptions!')
+
+}
+setup()
+// // Set up promotions
 
 
 app.use(express.static('public/'))
@@ -30,15 +47,19 @@ const getImages = () => {
 
 app.use(bodyParser.json())
 app.post('/addImageBase64', (req, res) => {
-    console.log('addImageBase64')
+    const firstLabel = req.body.labels.split(',')[0]
+    console.log('addImageBase64', firstLabel)
 
     addImage(req.body)
 
     // TODO Lookup from DB
 
+    client.hGet('active_promotions', firstLabel).then((promotion)=>{
+        console.log('Promotion found:', promotion)
 
-    io.emit('new_frame', req.body)
-    
+        io.emit('new_frame', {...req.body, promotion})
+    })
+
     res.sendStatus(204)
 })
 
--- ui/public/app.js
+++ ui/public/app.js
@@ -7,16 +7,33 @@ const toImg = (image) => {
 }
 
 var socket = io();
-socket.on('new_frame', function({image, location, labels}) {
+socket.on('new_frame', function({image, location, labels, promotion}) {
   // TODO also show location and labels
+  console.log(location, labels, promotion)
 
+
+  // TODO switch to handlebars?
   const imgNode = toImg(image)
+  const locationSpan = document.createElement('span')
+  locationSpan.innerText = location
+  const labelSpan = document.createElement('span')
+  labelSpan.innerText = labels
+  const promotionSpan = document.createElement('span')
+  promotionSpan.innerText = promotion
+
+  const containerDiv = document.createElement('span')
+  containerDiv.appendChild(locationSpan)
+  containerDiv.appendChild(labelSpan)
+  containerDiv.appendChild(promotionSpan)
+  containerDiv.appendChild(imgNode)
+
+
   const parent = document.getElementById('images')
 
   // Clear only if we want a single image
   if(true) { 
     parent.innerHTML = ''
   }
+  parent.appendChild(containerDiv)
 
-  parent.appendChild(imgNode)
 })
